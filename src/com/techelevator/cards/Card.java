4a1d6576d35c15a8656a21b3847b5383e9a9a1ec
Daniel Wintrich <danielwintrich@MacBook-Air.local>
2015-10-09 13:26:59 -0400

    Added Constants for suits

--- src/com/techelevator/cards/Card.java
+++ src/com/techelevator/cards/Card.java
@@ -4,6 +4,15 @@ public class Card {
 	private String suit;
 	private int rank;
 	
+	public static final String HEARTS = "hearts";
+	public static final String CLUBS = "clubs";
+	public static final String SPADES = "spades";
+	public static final String DIAMONDS = "diamonds";
+	public static final int ACE = 1;
+	public static final int KING = 13;
+	public static final int QUEEN = 12;
+	public static final int JACK = 11;
+	
 	public Card(int rank, String suit) {
 		this.rank = rank;
 		this.suit = suit;
--- src/com/techelevator/cards/CardsDemo.java
+++ src/com/techelevator/cards/CardsDemo.java
@@ -1,7 +1,10 @@
 package com.techelevator.cards;
 
+import static com.techelevator.cards.Card.*;
+
 public class CardsDemo {
 	public static void main(String[] args) {
-		
+		Card newCard = new Card(ACE, SPADES);
+		System.out.println("Your card is the "+newCard.getRank()+" of "+newCard.getSuit());
 	}
 }
